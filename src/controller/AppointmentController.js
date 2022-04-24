13cfa944fa550dd9acfeec06f4b605250166f3a5
João de Melo <joaomigueld3@gmail.com>
2022-04-23 22:34:10 -0300

    eslint configuration

--- src/controller/AppointmentController.js
+++ src/controller/AppointmentController.js
@@ -1,167 +1,165 @@
-import AppointmentModel from "../model/AppointmentModel.js";
-import bubbleSort from "../utils/bubbleSort.js";
-import sortByDate from "../utils/sortByDate.js";
+import AppointmentModel from '../model/AppointmentModel.js';
+import bubbleSort from '../utils/bubbleSort.js';
+import sortByDate from '../utils/sortByDate.js';
 
+class AppointmentController {
+  async index(request, response) {
+    try {
+      const app = await AppointmentModel.find();
+      if (app.length == 0) {
+        return response.status(404).json({ message: 'No appointments found in database' });
+      }
 
-class AppointmentController{
+      const aux = [];
+      for (let i = 0; i < app.length; i++) {
+        aux[i] = app[i].appDate;
+      }
+      console.log(aux);
+      // sorted dates
+      bubbleSort(aux);
+      console.log(aux);
+      const aux3 = [];
+      // array aux3 with objects sorted by date
+      sortByDate(aux, aux3, app);
+      const aux4 = aux3;
+      for (let i = 0; i < aux3.length; i++) {
+        const datetime = new Date(`${aux3[i].appDate.toString().slice(0, 11) + aux3[i].appTime}Z`);
+        console.log(datetime);
+        aux4[i].appDate = datetime;
+      }
+      console.log(aux4);
+      const aux5 = [];
+      for (let i = 0; i < aux4.length; i++) {
+        aux5[i] = aux4[i].appDate;
+      }
+      bubbleSort(aux5);
+      const aux6 = [];
+      sortByDate(aux5, aux6, aux4);
+      return response.send(aux6);
+    } catch (error) {
+      console.log(error.message);
+      response.status(400).send({ message: 'An unexpected error happened' });
+    }
+  }
 
-    async index(request,response){        
-        try{
-            const app = await AppointmentModel.find();
-            if(app.length==0){
-                return response.status(404).json({message:"No appointments found in database"});
+  async getOne(request, response) {
+    const { id } = request.params;
+
+    try {
+      const app = await AppointmentModel.findById(id);
+      if (app) {
+        return response.status(200).send(app);
+      }
+
+      return response.status(404).json({ message: 'appointment not found!' });
+    } catch (error) {
+      console.log(error.message);
+      response.status(400).send({ message: 'An unexpected error happened' });
+    }
+  }
+
+  async store(request, response) {
+    const {
+      name, cpf, email, birthDate, appDate, appTime, report,
+    } = request.body;
+
+    try {
+      const verifyApp = await AppointmentModel.findOne({ email });
+      const verifyCpf = await AppointmentModel.findOne({ cpf });
+      if (!verifyApp && !verifyCpf) {
+        const stringToDate = Date.parse(appDate);
+        const newDate = new Date(stringToDate);
+        console.log(appDate);
+        console.log(stringToDate);
+        console.log(newDate);
+        const checkDay = await AppointmentModel.find({ appDate: newDate });
+        console.log({ checkDay });
+        // if someone already booked an app this day
+        if (checkDay) {
+          for (let i = 0; i < checkDay.length; i++) {
+            console.log(checkDay);
+            const appTimeNew = checkDay[i].appTime;
+            console.log(checkDay[i].appTime);
+            console.log(appTime);
+            console.log(appTimeNew);
+            if (appTime === appTimeNew) {
+              return response.status(400).send(
+                { message: 'another appointment is already booked for this day and time' },
+              );
             }
-            else{  
-                const aux=[]
-                for(let i=0; i<app.length; i++){
-                    aux[i]=app[i].appDate;
-                }
-                console.log(aux);
-                //sorted dates
-                bubbleSort(aux);
-                console.log(aux);
-                const aux3=[];
-                // array aux3 with objects sorted by date
-                sortByDate(aux,aux3,app)
-                const aux4=aux3;                               
-                for(var i=0; i<aux3.length; i++){
-                    var datetime = new Date(aux3[i].appDate.toString().slice(0,11) + aux3[i].appTime + 'Z');
-                    console.log(datetime)
-                    aux4[i].appDate=datetime
-                }                
-                console.log(aux4)                
-                const aux5=[];
-                for(let i=0; i<aux4.length; i++){
-                    aux5[i]=aux4[i].appDate;
-                }
-                bubbleSort(aux5)
-                const aux6=[];
-                sortByDate(aux5,aux6,aux4)
-                return response.send(aux6);
-            }
-            
-        }catch(error){
-            console.log(error.message);
-            response.status(400).send({ message: "An unexpected error happened" });
+          }
         }
-    }
 
-    async getOne(request,response){
-        const id = request.params.id;
+        const app = await AppointmentModel.create({
+          name,
+          cpf,
+          email,
+          birthDate,
+          appDate,
+          appTime,
+          isSolved: false,
+          report,
+        });
+        return response.status(200).send({ message: 'Appointment registred with success', app });
+      }
+      return response.status(404).json({ message: 'email or CPF already being used' });
+    } catch (error) {
+      if (error.name === 'ValidationError') {
+        const errors = {};
 
-            try{
-                const app = await AppointmentModel.findById(id);
-                if(app){
-                    return response.status(200).send(app);
-                }
-                else{
-                    return response.status(404).json({message:"appointment not found!"});
-                }
-            }catch(error){
-                    console.log(error.message);
-                    response.status(400).send({ message: "An unexpected error happened" });
-            }
+        Object.keys(error.errors).forEach((key) => {
+          errors[key] = error.errors[key].message;
+        });
+        console.log(errors);
+        return response.status(400).send({ message: JSON.stringify(errors) });
+      }
+      console.log(error.message);
+      response.status(400).send({ message: error.message });
     }
-    async store(request,response){
-        const{name, cpf, email, birthDate, appDate,appTime, report}=request.body;
+  }
 
-            try{
-                const verifyApp = await AppointmentModel.findOne({email});
-                const verifyCpf = await AppointmentModel.findOne({cpf});
-                if(!verifyApp && !verifyCpf){
-                    const stringToDate= Date.parse(appDate)
-                    const newDate = new Date(stringToDate)
-                    console.log(appDate)
-                    console.log(stringToDate)
-                    console.log(newDate)
-                    const checkDay = await AppointmentModel.find({appDate: newDate});
-                    console.log({checkDay});
-                        //if someone already booked an app this day
-                        if(checkDay){
-                            for(let i=0; i<checkDay.length;i++){
-                            console.log(checkDay);
-                            const appTimeNew=checkDay[i].appTime;
-                            console.log(checkDay[i].appTime)
-                            console.log(appTime);
-                            console.log(appTimeNew);
-                            if(appTime===appTimeNew){
-                                return response.status(400).send(
-                                    {message:"another appointment is already booked for this day and time"});
-                            }
-                            }
-                            
-                        }
-                    
-                    const app = await AppointmentModel.create({
-                        name,
-                        cpf,
-                        email,
-                        birthDate,
-                        appDate,
-                        appTime,
-                        isSolved:false,
-                        report,
-                    });
-                    return response.status(200).send({message:"Appointment registred with success", app});
-                }else{
-                    return response.status(404).json({message:"email or CPF already being used"});
-                }
-            }catch(error){                
-                if (error.name === "ValidationError") {
-                    let errors = {};
-              
-                    Object.keys(error.errors).forEach((key) => {
-                      errors[key] = error.errors[key].message;
-                    });
-                    console.log(errors)
-                    return response.status(400).send({message: JSON.stringify(errors)});
-                  }
-                console.log(error.message);
-                response.status(400).send({message: error.message});
-        }
+  async remove(request, response) {
+    const { id } = request.params;
+    try {
+      const verifyApp = await AppointmentModel.findById(id);
+      if (verifyApp) {
+        await verifyApp.remove();
+        return response.status(200).send({ message: 'Appointment removed' });
+      }
+      response.status(404).send({ message: "Appointment not found, therefore can't be removed" });
+    } catch (error) {
+      console.log(error.message);
+      response.status(400).send({ message: 'An unexpected error happened' });
     }
-    async remove(request,response){
-        const id = request.params.id;
-        try{
-            const verifyApp = await AppointmentModel.findById(id);
-            if(verifyApp){
-                await verifyApp.remove();
-                return response.status(200).send({ message: "Appointment removed" });
-            }else{
-                response.status(404).send({ message: "Appointment not found, therefore can't be removed" });
-            }
+  }
 
-        }catch(error){
-            console.log(error.message);
-            response.status(400).send({ message: "An unexpected error happened" });
-        }
+  async update(request, response) {
+    const { id } = request.params;
+    const { isSolved, report } = request.body;
+    try {
+      if (report.length > 30) {
+        return response.status(404).send({ message: 'Report exceeds maximum length of 30' });
+      }
+      const verifyApp = await AppointmentModel.findById(id);
+      if (verifyApp) {
+        const app = await AppointmentModel.findByIdAndUpdate(
+          id,
+          {
+            isSolved,
+            report,
+          },
+          {
+            new: true,
+          },
+        );
+        return response.status(200).send({ message: 'Appointment updated with success', app });
+      }
+      return response.status(404).send({ message: "Appointment not found, therefore can't be updated" });
+    } catch (error) {
+      console.log(error.message);
+      response.status(400).send({ message: error.message });
     }
-    async update(request,response){
-        const id=request.params.id;
-        const{isSolved, report}=request.body;
-        try{
-            if(report.length>30){
-                return response.status(404).send({message: "Report exceeds maximum length of 30"})
-            }
-            const verifyApp = await AppointmentModel.findById(id);
-            if(verifyApp){
-               const app = await AppointmentModel.findByIdAndUpdate(id,
-                    {
-                        isSolved,
-                        report,
-                    },
-                    {
-                        new:true,
-                    });
-                    return response.status(200).send({message:"Appointment updated with success",app});
-            }   else{
-                   return response.status(404).send({ message: "Appointment not found, therefore can't be updated" });
-            }
-        }catch(error){
-            console.log(error.message);
-            response.status(400).send({message: error.message});
-    }
-    }
+  }
 }
 
 export default AppointmentController;
--- src/index.js
+++ src/index.js
@@ -1,35 +1,34 @@
-import express from "express";
-import morgan from "morgan";
-import mongoose from "mongoose";
-import dotenv from "dotenv";
-import cors from "cors";
-import helmet from "helmet";
+import express from 'express';
+import morgan from 'morgan';
+import mongoose from 'mongoose';
+import dotenv from 'dotenv';
+import cors from 'cors';
+import helmet from 'helmet';
 
+import AppointmentRouter from './router/AppointmentRouter.js';
 
-dotenv.config({path:'.env.example'});
+dotenv.config({ path: '.env.example' });
 
-import AppointmentRouter from "./router/AppointmentRouter.js"
-
-const {DATABASE_URL, PORT} = process.env;
+const { DATABASE_URL, PORT } = process.env;
 mongoose
   .connect(DATABASE_URL)
   .then(() => {
-    console.log("Database Connected...");
+    console.log('Database Connected...');
   })
   .catch((error) => {
-    console.error("Error to connect to database: " + error.message);
+    console.error(`Error to connect to database: ${error.message}`);
   });
 
-  const app = express();
+const app = express();
 
 app.use(cors());
 app.use(helmet());
 
 app.use(express.json());
-app.use(morgan("dev"));
+app.use(morgan('dev'));
 
 app.use(AppointmentRouter);
 
 app.listen(PORT, () => {
-    console.log(`Server running on PORT ${PORT}`);
-  });
+  console.log(`Server running on PORT ${PORT}`);
+});
